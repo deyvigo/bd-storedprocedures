c22f3142808bf931ded8d02cbca49cf1d3f8b875
deyvigo <deyvi.gomez@unmsm.edu.pe>
2024-11-13 00:00:58 -0500

    feat: client payment-method

--- src/controllers/client.py
+++ src/controllers/client.py
@@ -1,7 +1,9 @@
 from flask import request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from pydantic import ValidationError
 
 from services.database import Database
+from dto.metodo_pago import MetodoPagoCreateDTO
 
 class ClientController:
   @staticmethod
@@ -25,3 +27,74 @@ class ClientController:
     del response['password']
 
     return jsonify(response), 200
+  
+  @staticmethod
+  @jwt_required()
+  def get_all_client_payment_methods():
+    client = get_jwt_identity()
+    db = Database().connection()
+
+    try:
+      with db.cursor() as cursor:
+        cursor.callproc('sp_get_metodo_pago_all_active_by_id_client', [client['id_cliente']])
+        response = cursor.fetchall()
+    except Exception as e:
+      return jsonify({'error': f'No se pudo obtener los métodos de pago del cliente. {e}'}), 500
+    finally:
+      db.close()
+
+    if not response:
+      return jsonify({ 'error': 'No tienes métodos de pago' }), 404
+    
+    return jsonify(response), 200
+  
+  @staticmethod
+  @jwt_required()
+  def delete_client_payment_method(id_metodo_pago):
+    client = get_jwt_identity()
+    db = Database().connection()
+
+    try:
+      with db.cursor() as cursor:
+        cursor.callproc('sp_delete_metodo_pago_by_id', [id_metodo_pago, 0, ''])
+        cursor.execute("SELECT @_sp_delete_metodo_pago_by_id_1 AS rows_affected, @_sp_delete_metodo_pago_by_id_2 AS error_message;")
+        result = cursor.fetchone()
+    except Exception as e:
+      return jsonify({'error': f'No se pudo eliminar el método de pago. {e}'}), 500
+    finally:
+      db.close()
+
+    if result['error_message']:
+      return jsonify({ 'error': result['error_message'] }), 400
+    
+    return jsonify({ 'message': 'Método de pago eliminado' }), 200
+  
+  @staticmethod
+  @jwt_required()
+  def create_client_payment_method():
+    client = get_jwt_identity()
+    db = Database().connection()
+
+    try:
+      data = MetodoPagoCreateDTO(**request.json)
+    except ValidationError as ve:
+      return jsonify({'error': ve.errors() }), 400
+    
+    # replace values of 7-12 digits with *
+    numero = data.numero_tarjeta
+    data.numero_tarjeta = numero[:6] + 'X' * 6 + numero[-4:]
+    
+    try:
+      with db.cursor() as cursor:
+        cursor.callproc("sp_register_metodo_pago", [data.nombre, data.numero_tarjeta, client["id_cliente"], 0, 0, ''])
+        cursor.execute("SELECT @_sp_register_metodo_pago_4 AS rows_affected, @_sp_register_metodo_pago_5 AS last_id, @_sp_register_metodo_pago_6 AS error_message;")
+        result = cursor.fetchone()
+    except Exception as e:
+      return jsonify({'error': f'No se pudo registrar el método de pago. {e}'}), 500
+    finally:
+      db.close()
+
+    if result['error_message']:
+      return jsonify({ 'error': result['error_message'] }), 400
+    
+    return jsonify({ 'message': 'Método de pago registrado' }), 201
--- src/routes/client.py
+++ src/routes/client.py
@@ -16,7 +16,135 @@ def get_all_client_info():
   responses:
     200:
       description: Información del cliente
+      schema:
+        type: object
+        properties:
+          apellido_pat:
+            type: string
+            example: "Apellido Paterno"
+          apellido_mat:
+            type: string
+            example: "Apellido Materno"
+          correo:
+            type: string
+            example: "correo@correo.com"
+          dni:
+            type: string
+            example: "12345678"
+          fecha_nacimiento:
+            type: string
+            example: "2000-01-01"
+          id_cliente:
+            type: integer
+            example: 1
+          nombre:
+            type: string
+            example: "Nombre"
+          sexo:
+            type: string
+          telefono:
+            type: string
+            example: "123456789"
+          username:
+            type: string
+            example: "username"
     401:
       description: No autorizado
   """
   return ClientController.get_all_client_info()
+
+@client_router.route('/client/payment-methods', methods=['GET'])
+def get_all_client_payment_methods():
+  """
+  Obtener los métodos de pago del cliente
+  ---
+  tags:
+    - Protegido
+  security:
+    - BearerAuth: []
+  responses:
+    200:
+      description: Métodos de pago del cliente
+      schema:
+        type: array
+        items:
+          type: object
+          properties:
+            id_metodo_pago:
+              type: integer
+              example: 1
+            estado:
+              type: enum
+              values:
+                - activo
+                - inactivo
+              example: "activo"
+            nombre:
+              type: string
+              example: "Tarjeta 1"
+            numero_tarjeta:
+              type: string
+              example: "123456XXXXXX3456"
+            id_cliente:
+              type: integer
+              example: 1
+    404:
+      description: No tienes métodos de pago
+  """
+  return ClientController.get_all_client_payment_methods()
+
+@client_router.route('/client/payment-methods/<int:id_metodo_pago>', methods=['PUT'])
+def delete_client_payment_method(id_metodo_pago):
+  """
+  Eliminar un método de pago del cliente
+  ---
+  tags:
+    - Protegido
+  security:
+    - BearerAuth: []
+  parameters:
+    - in: path
+      name: id_metodo_pago
+      required: true
+      schema:
+        type: integer
+        example: 1
+  responses:
+    200:
+      description: Método de pago eliminado
+      content:
+        application/json:
+          example:
+            message: "Método de pago eliminado"
+    400:
+      description: Error al eliminar el método de pago
+  """
+  return ClientController.delete_client_payment_method(id_metodo_pago)
+
+@client_router.route('/client/payment-method', methods=['POST'])
+def create_client_payment_method():
+  """
+  Crear un método de pago del cliente
+  ---
+  tags:
+    - Protegido
+  security:
+    - BearerAuth: []
+  parameters:
+    - in: body
+      name: body
+      required: true
+      schema:
+        type: object
+        properties:
+          nombre:
+            type: string
+            example: "Tarjeta 1"
+          numero_tarjeta:
+            type: string
+            example: "123456XXXXXX3456"
+  responses:
+    201:
+      description: Método de pago registrado
+  """
+  return ClientController.create_client_payment_method()
