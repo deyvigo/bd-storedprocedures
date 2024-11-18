32f3fd8e81e4841c422d33cb4455328359ce887b
deyvigo <deyvi.gomez@unmsm.edu.pe>
2024-11-17 22:07:22 -0500

    feat: create transaction pdf prev

--- src/routes/transaccion.py
+++ src/routes/transaccion.py
@@ -35,3 +35,42 @@ def get_all_transactions_by_id_client():
               example: 100
   """
   return TransaccionController.get_all_transactions_by_id_client()
+
+@transaccion_router.route('/transaction/pdf/<int:id_transaccion>/generate', methods=['GET'])
+def create_pdf_transaction(id_transaccion):
+  """
+  Generar el PDF de la transaccion
+  ---
+  tags:
+    - Protegido
+  security:
+    - BearerAuth: []
+  parameters:
+    - in: path
+      name: id_transaccion
+      required: true
+      schema:
+        type: integer
+        example: 1
+  responses:
+    200:
+      description: PDF de la transaccion
+    404:
+      description: No se ha encontrado el id seleccionado
+  """
+  return TransaccionController.create_pdf_transaction(id_transaccion)
+
+@transaccion_router.route('/transaction/pdf/<string:name>', methods=['GET'])
+def get_pdf_transaction(name):
+  """
+  Redirecciona al recurso PDF
+  ---
+  parameters:
+    - in: path
+      name: name
+      required: true
+      schema:
+        type: string
+        example: "hash"
+  """
+  return TransaccionController.get_pdf_transaction(name)
