0cdefda515a60bcf1d38c6c0dd5e13ca87fe100b
Giovanni Coronel <elvis.coronel@unmsm.edu.pe>
2024-11-20 01:45:02 -0500

    add:terminal , bus,bus seat,routes ,parada-intermedia

--- src/dto/TerminalDTO.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, constr, confloat
-class TerminalCreateDTO(BaseModel): 
-  nombre: constr(min_length=4, max_length=255)
-  departamento: constr(min_length=3, max_length=100)
-  provincia: constr(min_length=3, max_length=100)
-  
--- src/routes/admin.py
+++ src/routes/admin.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
-from controllers.admin import AdminController
+from controllers.adminCreate import AdminController
 
 admin_router = Blueprint('admin', __name__)
 
 @admin_router.route('/admin/terminal', methods=['POST'])
-def create_admin_terminal():
+def admin_create_terminal():
   """
   Crear una terminal
   ---
@@ -31,16 +31,141 @@ def create_admin_terminal():
   responses:
     201:
       description: Terminal creada exitosamente
-      schema:
-        type: object
   """
-  return AdminController.create_admin_terminal()
+  return AdminController.admin_create_terminal()
 
-#ruta para crear bus
 @admin_router.route('/admin/bus', methods=['POST'])
 def admin_create_bus():
   """
-  
-  
+  Crear un bus
+  ---
+  tags:
+    - Protegido
+  security:
+    - BearerAuth: []
+  parameters:
+    - in: body
+      name: body
+      required: true
+      schema:
+        type: object
+        properties:
+          asientos:
+            type: integer
+            example: 40
+          placa:
+            type: string
+            example: "BCD123"
+          marca:
+            type: string
+            example: "Mercedes-Benz"
+          niveles:
+            type: integer
+            example: 2
+          id_tipo_servicio_bus:
+            type: integer
+            example: 1        
+  responses:
+    201:
+      description: Bus creado exitosamente
   """
   return AdminController.admin_create_bus()
+
+@admin_router.route('/admin/asiento', methods=['POST'])
+def admin_create_asiento():
+  """
+  Crear un asiento
+  ---
+  tags:
+    - Protegido
+  security:
+    - BearerAuth: []
+  parameters:
+    - in: body
+      name: body
+      required: true
+      schema:
+        type: object
+        properties:
+          nivel:
+            type: integer
+            example: 1
+          numero:
+            type: integer
+            example: 25
+          id_bus:
+            type: integer
+            example: 1
+  responses:
+    201:
+      description: Asiento creado exitosamente
+  """
+  return AdminController.admin_create_asiento()
+
+@admin_router.route('/admin/ruta', methods=['POST'])
+def admin_create_route():
+  """
+  Crear una ruta
+  ---
+  tags:
+    - Protegido
+  security:
+    - BearerAuth: []
+  parameters:
+    - in: body
+      name: body
+      required: true
+      schema:
+        type: object
+        properties:
+          duracion_estimada:
+            type: time
+            example: "04:00:00"
+          distancia:
+            type: number
+            example: 1200
+          estado:
+            type: string
+            example: "Activo"
+          id_origen:
+            type: integer
+            example: 1
+          id_destino:
+            type: integer
+            example: 2
+  responses:
+    201:
+      description: Ruta creada exitosamente
+  """
+  return AdminController.admin_create_route()
+
+@admin_router.route('/admin/parada-intermedia', methods=['POST'])
+def admin_create_parada():
+  """
+  Crear una parada intermedia
+  ---
+  tags:
+    - Protegido
+  security:
+    - BearerAuth: []
+  parameters:
+    - in: body
+      name: body
+      required: true
+      schema:
+        type: object
+        properties:
+          ordinal:
+            type: integer
+            example: 1
+          id_terminal:
+            type: integer
+            example: 1
+          id_ruta:
+            type: integer
+            example: 2
+  responses:
+    201:
+      description: Parada intermedia creada exitosamente
+  """
+  return AdminController.admin_create_parada()
