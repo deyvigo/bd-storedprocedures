bb4f69fd8206895bfb4927cd73a25a364d0eb4d3
ProtoSG <diegoalbertosalazar177@gmail.com>
2024-11-21 08:45:44 -0500

    feat: import terminal_route in app

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import JWTManager, jwt_required
 from flasgger import Swagger
 
 from services.database import Database
-from routes import signup, login, client_router, ticket_router, transaccion_router, discount_router, service_router,general_router
+from routes import signup, login, client_router, ticket_router, transaccion_router, discount_router, service_router,general_router, terminal_router
 
 app = Flask(__name__)
 
@@ -38,6 +38,7 @@ app.register_blueprint(transaccion_router)
 app.register_blueprint(discount_router)
 app.register_blueprint(service_router)
 app.register_blueprint(general_router)
+app.register_blueprint(terminal_router)
 
 @app.route('/helloworld/public', methods=['GET'])
 def helloworld():
